78ac8f4f381c9f771a522a0e71946048517867b0
hayamhiba <es-hayam.ibrahim2023@alexu.edu.eg>
2023-05-08 01:59:27 +0300

    modify routes

--- Backend/routes/display_routes.js
+++ Backend/routes/display_routes.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
     var displayHandlers = require('../Controllers/DisplayController');
     // List Routes
-    app.route('/author')
+    app.route('/author/:authorId')
         .get(displayHandlers.get_author);
-    app.route('/book')
+    app.route('/book/:bookId')
         .get(displayHandlers.get_book);
 };
