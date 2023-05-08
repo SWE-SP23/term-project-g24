4df5d8828bbd6f80f0e3f06f691caba0532ff297
hayamhiba <es-hayam.ibrahim2023@alexu.edu.eg>
2023-05-08 20:02:13 +0300

    remove merge signs

--- Backend/routes/userpage_routes.js
+++ Backend/routes/userpage_routes.js
@@ -2,16 +2,9 @@ module.exports = function(app) {
     var userHandlers = require('../Controllers/UserpageController');
     // List Routes
     app.route('/all/:userId')
-<<<<<<< HEAD
-        .post(userHandlers.get_all_books);
-    app.route('/:userId/:bookId')
-        .post(userHandlers.addToBooks);
-   app.route('/remove/:userId/:bookId')
-=======
         .get(userHandlers.get_all_books);
     app.route('/:userId/:bookId')
         .post(userHandlers.addToBooks);
     app.route('/remove/:userId/:bookId')
->>>>>>> merge-branch
         .post(userHandlers.removeFromBooks);
 };
